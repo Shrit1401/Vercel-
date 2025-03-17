03cac6748af5ff66adcc5a73fe25149909126a12
Shrit Shrivastava <67160842+Shrit1401@users.noreply.github.com>
2025-03-17 17:47:54 +0530

    Update aws.ts

--- vercel-upload-service/src/aws.ts
+++ vercel-upload-service/src/aws.ts
@@ -3,12 +3,11 @@ import fs from "fs";
 
 // replace with your own credentials
 const s3 = new S3({
-  accessKeyId: "316a1601c97ba28ae7df61fb4baaf4ed",
+  accessKeyId: "main nhi bata rha",
   secretAccessKey:
-    "d54d6fc5b056eb373fe550b4693f95c60de8f713b1b672e941f6f54dab094f87",
-  endpoint: "https://2d98073c5d0cf875919ade03091d9795.r2.cloudflarestorage.com",
+    "main nhi bata rha",
+  endpoint: "main nhi bata rha",
 });
-
 // fileName => output/12312/src/App.jsx
 // filePath => /Users/harkiratsingh/vercel/dist/output/12312/src/App.jsx
 export const uploadFile = async (fileName: string, localFilePath: string) => {
